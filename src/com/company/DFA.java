37b91698f17917737cd5f61b48935eee52087f96
Akhil365 <akhilkanagala18@gmail.com>
2023-02-22 20:12:45 -0600

    working intersection

--- src/com/company/DFA.java
+++ src/com/company/DFA.java
@@ -28,23 +28,84 @@ public class DFA {
         this.finalStates = finalStates;
     }
 
-    public static DFA intersection (DFA m1, DFA m2) {
-        int[][] intersectionTable = new int[m1.transitionTable.length][2];
-        int[] newFinalStates = new int[0];
+    public int[][] getTransitionTable () {
+        return this.transitionTable;
+    }
 
-        for (int i = 0; i < intersectionTable.length; i++) {
-            for (int j = 0; j < 2; j++) {
-                int newState1 = m1.transitionTable[i][j];
-                int newState2 = m2.transitionTable[i][j];
-                intersectionTable[i][j] = newState1 == newState2 ? newState1 : -1;
+    public int[] getFinalStates () {
+        return this.finalStates;
+    }
+
+    public static DFA intersection (DFA m1, DFA m2) {
+        // Get the transition tables and final states of the two DFAs
+        int[][] transitionTableM1 = m1.getTransitionTable();
+        int[] finalStatesM1 = m1.getFinalStates();
+        int[][] transitionTableM2 = m2.getTransitionTable();
+        int[] finalStatesM2 = m2.getFinalStates();
+
+        // Calculate the number of states in the new DFA
+        int numStates = transitionTableM1.length * transitionTableM2.length;
+
+        // Create a new transition table and final states array for the new DFA
+        int[][] tt3 = new int[numStates][2];
+
+        int numFinalStates = 10;
+        /*for (int f : finalStatesM1) {
+            for (int g : finalStatesM2) {
+                if (f * transitionTableM2.length + g < numStates) {
+                    numFinalStates++;
+                }
             }
-            if (Arrays.binarySearch(m1.finalStates, i) >= 0 && Arrays.binarySearch(m2.finalStates, i) >= 0) {
-                newFinalStates = Arrays.copyOf(newFinalStates, newFinalStates.length + 1);
-                newFinalStates[newFinalStates.length - 1] = i;
+        }*/
+        System.out.println(" Final States Count:" + numFinalStates);
+        int[] fs3 = new int[numFinalStates];
+
+        // Calculate the transition table and final states of the new DFA
+        int k = 0;
+        for (int i = 0; i < transitionTableM1.length; i++) {
+            for (int j = 0; j < transitionTableM2.length; j++) {
+                // Calculate the new state based on the combination of the two states
+                System.out.println("Testing i: " + i + " and j: " + j);
+
+                int newState = i * transitionTableM2.length + j;
+
+                // Calculate the transition for input 0
+                int t1 = transitionTableM1[i][0];
+                int t2 = transitionTableM2[j][0];
+                int newT = t1 * transitionTableM2.length + t2;
+                tt3[newState][0] = newT;
+
+                // Calculate the transition for input 1
+                t1 = transitionTableM1[i][1];
+                t2 = transitionTableM2[j][1];
+                newT = t1 * transitionTableM2.length + t2;
+                tt3[newState][1] = newT;
+
+                // Check if the new state is an accepting state
+                boolean isFinal1 = false;
+                boolean isFinal2 = false;
+                for (int f : finalStatesM1) {
+                    if (i == f) {
+                        isFinal1 = true;
+                        break;
+                    }
+                }
+                for (int f : finalStatesM2) {
+                    if (j == f) {
+                        isFinal2 = true;
+                        break;
+                    }
+                }
+                if (isFinal1 && isFinal2) {
+                    System.out.println("New Final State: \n ");
+                    fs3[k++] = newState;
+                }
             }
         }
 
-        return new DFA(intersectionTable, newFinalStates);
+        // Create the new DFA and return it
+        DFA m3 = new DFA(tt3, Arrays.copyOf(fs3, k));
+        return m3;
     }
 
     public boolean run (String input) {
@@ -63,15 +124,14 @@ public class DFA {
             else if (c == '1') {
                 state = transitionTable[state][1];
             }
-
-            state++;
         }
 
         //if the state we have reached is equal to the final state of the DFA, return true
-        if (finalStates[0] == state) {
-            return true;
+        for (int finalState : finalStates) {
+            if (finalState == state) {
+                return true;
+            }
         }
-
         return false;
     }
 }
